dc4758904cbfe23747a2f4a9e2a921573f4841df
Jeff Longo <jeffreylongo@yahoo.com>
2017-03-20 13:56:44 -0400

    something

---
+++ variables/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string helloWorld = "Hello World";
+            int columbus = 1492;
+            bool dog = true;
+            DateTime date = DateTime.Now;
+            string dateWithFormat = date.ToLongDateString();
+            decimal dec = 5.45m;
+            char x = 'x';
+
+            Console.WriteLine(helloWorld);
+            Console.ReadLine();
+
+            Console.WriteLine("Columbus sailed the world in " + columbus);
+            Console.ReadLine();
+
+            Console.WriteLine("It is {0} that I have dogs at home.", dog);
+            Console.ReadLine();
+
+            Console.WriteLine("Today is " + DateTime.Now);
+            Console.ReadLine();
+
+            Console.WriteLine("I spent " + dec + " on a cheeseburger.");
+            Console.ReadLine();
+
+            Console.WriteLine(x + " marks the spot");
+            Console.ReadLine();
+        }
+    }
+}
